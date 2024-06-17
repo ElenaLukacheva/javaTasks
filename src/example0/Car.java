73ab29ef1ea51b05ee255c8996e3766b0043685c
ElenaLukacheva <elenaly4@gmail.com>
2024-06-17 06:37:18 +0300

    added ExampleSimple

--- src/example0/Car.java
+++ src/example0/Car.java
@@ -1,13 +1,17 @@
 package example0;
 
+import java.util.Objects;
+
 public class Car {
     private String model;
     private Integer fuel;
+    private Double speed;
 
     public Car(String model, Integer fuel) {
         this.model = model;
         this.fuel = fuel;
         System.out.println("Создали машину " + model);
+        speed = 60.5;
     }
 
     public Car() {
@@ -18,10 +22,17 @@ public class Car {
     }
     public void goToRoad(){
         if (fuel > 20){
-            System.out.println("Maшина поехала на трассу");
-        }else {
+            System.out.println("Maшина поехала на трассу, остаток бензина " + fuel);
+            fuel = fuel - 15;
+        } else {
             System.out.println("Недостаточно бензина");
+            addFuelToCar();
+            System.out.println("Машина заправлена, " + fuel + " Литров");
         }
+    }
+
+    private void addFuelToCar(){
+        fuel = fuel + 20;
     }
 
     public void setFuel(Integer fuel) {
@@ -31,6 +42,28 @@ public class Car {
     public void beep(){
         System.out.println("Машина посигналила");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(model, car.model) && Objects.equals(fuel, car.fuel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, fuel);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", fuel=" + fuel +
+                '}';
+    }
+    
 
     public String getModel() {
         return model;
---
+++ src/example0/Main.java
@@ -0,0 +1,48 @@
+package example0;
+
+import example0.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car("Nissan Qashqa", 30);
+        nissan.goToRoad();
+        nissan.goToRoad();
+        nissan.goToRoad();
+        nissan.goToRoad();
+        System.out.println("---------------");
+
+        Car nissan1 = new Car("Nissan", 40);
+        Car nissan2 = new Car("Nissan", 40);
+        Car bmw = new Car("BMW", 80);
+        boolean isSame = nissan1.equals(nissan2);
+
+        boolean isModelSame = nissan1.getModel().equals(nissan2.getModel());
+        boolean isFuelSame = nissan1.getFuel().equals(nissan2.getFuel());
+        System.out.println(isModelSame);
+        System.out.println(isFuelSame);
+        System.out.println(nissan1);
+        System.out.println(nissan2);
+        System.out.println(bmw);
+        System.out.println(isSame);
+
+        System.out.println("---------------");
+        Car car = new Car();
+        car.beep();
+        car.setModel("Nissan");
+        car.setFuel(100);
+        car.goToRoad();
+        System.out.println(car.getModel());
+
+        Car car2 = new Car();
+        car2.setModel("BMW");
+        car2.setFuel(20);
+        car2.goToRoad();
+        System.out.println("BMW");
+
+        Car mers = new Car("Mersedez Benz", 100);
+        System.out.println(mers.getModel());
+        mers.goToRoad();
+
+
+    }
+}
